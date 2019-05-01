98865fa294f0f998f6e537f8057ebf188adaa1af
caneregin <cnrgn96@gmail.com>
2019-05-01 17:22:35 +0300

    Revert "Update HomeController.java"
    This reverts commit c1ef5f3f868a4e7de62f4fd6795cda246a17b475.

--- TeamBravoWiki/src/main/java/com/aydin/demo/teambravowiki/controller/HomeController.java
+++ TeamBravoWiki/src/main/java/com/aydin/demo/teambravowiki/controller/HomeController.java
@@ -60,7 +60,6 @@ public class HomeController {
             session.setAttribute("userId",userid );
             session.setAttribute("loginned", true);
             UserPageContext upc = userPageClient.getPageContext(Integer.parseInt(userid));
-            session.setAttribute("userImage", userImageClient.getUserImage(Integer.parseInt(userid)));
             session.setAttribute("userDetails", userPageClient.getPageContext(Integer.parseInt(userid)));
             session.setAttribute("userImage", userImageClient.getUserImage(Integer.parseInt(userid)));
             return "redirect:/home";
