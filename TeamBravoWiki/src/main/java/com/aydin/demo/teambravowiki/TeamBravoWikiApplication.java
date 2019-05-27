465de6cbbbe6e2b398b679967df04959ece5bd0b
ErtugrulAcar <ertugrulacar1996@gmail.com>
2019-05-27 14:34:50 +0300

    Deploy
    Sitemiz deploy edildi
    pom.xml e atılan 2 dependency ile deploy sağlandı

--- TeamBravoWiki/src/main/java/com/aydin/demo/teambravowiki/TeamBravoWikiApplication.java
+++ TeamBravoWiki/src/main/java/com/aydin/demo/teambravowiki/TeamBravoWikiApplication.java
@@ -3,7 +3,7 @@ package com.aydin.demo.teambravowiki;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
-import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
 
 @SpringBootApplication
 public class TeamBravoWikiApplication extends SpringBootServletInitializer {
